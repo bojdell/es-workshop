22e0560425270a6323c6a17f0dc91f2bc22e7faf
Bodecker DellaMaria <bojdell@gmail.com>
2016-07-17 15:51:46 -0700

    add basic frontend

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('Server listening on port 3000.');
+});
