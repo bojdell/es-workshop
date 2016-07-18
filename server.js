e57f39256b45ec96b75b73dc9cfbcc2bb896ffad
Bodecker DellaMaria <bojdell@gmail.com>
2016-07-18 00:25:10 -0700

    serve solutions (if you've got em ;)

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+app.use(express.static('solutions'));
 
 app.listen(3000, function () {
   console.log('Server listening on port 3000.');
