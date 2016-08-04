c09479b0adb6b517c1b14c3a9385ba3b3732e94f
Bodecker DellaMaria <bojdell@gmail.com>
2016-08-03 21:02:17 -0700

    better web server (remove node & express dependencies)

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static('public'));
-app.use(express.static('solutions'));
-
-app.listen(3000, function () {
-  console.log('Server listening on port 3000.');
-});
---
+++ server.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/ruby
+
+system('ruby -run -ehttpd public -p 3000')
